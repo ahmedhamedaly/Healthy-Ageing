eeae5b9b605dfbb720ab54f5f4fb30bc2623f5f9
Ahmed <ahmedhamedaly19@gmail.com>
2020-03-11 17:30:08 +0000

    added login/register and dependences

--- lib/login.dart
+++ lib/login.dart
@@ -6,22 +6,22 @@ class Login extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-          home: Scaffold(
-            appBar: AppBar(
-              title: Text('Log In'),
-            ),
-            body: Center(
-              child: RaisedButton(
-                onPressed: () {
-                  Navigator.of(context)
-                      .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
-                        return new MyHomePage(title: '');
-                      }));
-                },
-                child: Text('Homepage'),
-              ),
-            ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Log In'),
+        ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
+                return new MyHomePage(title: '');
+              }));
+            },
+            child: Text('Homepage'),
           ),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:Healthy_Ageing/screens/wrapper.dart';
+import 'package:Healthy_Ageing/services/auth.dart';
 import 'package:flutter/material.dart';
-import './login.dart';
+import 'package:provider/provider.dart';
+import 'package:Healthy_Ageing/models/user.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,13 +10,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Swipe',
-      theme: ThemeData(
-        primaryColorBrightness: Brightness.light,
-        primarySwatch: Colors.blue,
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
       ),
-      home: Login(),
     );
   }
 }
