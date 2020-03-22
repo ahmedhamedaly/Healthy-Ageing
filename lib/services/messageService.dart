1c935f7d14772ae02378946f71f18d8a34804b85
Mushman2 <hartigam@tcd.ie>
2020-03-22 14:53:51 +0000

    Committing minor changes

--- lib/services/messageService.dart
+++ lib/services/messageService.dart
@@ -1,13 +1,29 @@
 
 
+import 'package:Healthy_Ageing/models/chat_repo.dart';
+import 'package:Healthy_Ageing/models/message.dart';
+
 class MessageService{
 
-  Future sendMessage() async{
-
+  Future sendMessage(String chatroomId, String text) async{
+    final User user = await UserRepo.getInstance().getCurrentUser();
+    final bool success = await ChatRepo.getInstance().sendMessageToChatroom(chatroomId, user, text);
+    if (!success) {
+      dispatch(MessageSendErrorEvent());
+    }
   }
 
-  Future recieveMessage() async{
-
+  Future getMessages() async{
+    final User user = await UserRepo.getInstance().getCurrentUser();
+    chatroomSubscription = ChatRepo.getInstance().getMessagesForChatroom(chatroomId).listen((chatroom) {
+      if (chatroom != null) {
+        List<Message> processedMessages = chatroom.messages
+            .map((message) => Message(
+            message.author, message.timestamp, message.value, message.author.uid == user.uid))
+            .toList();
+        dispatch(MessageReceivedEvent(processedMessages));
+      }
+    });
   }
 
 }
