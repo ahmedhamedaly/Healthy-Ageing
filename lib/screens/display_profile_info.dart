67557b282617f0496d2a827e56f8453816a519ab
alannahhenry <60479099+alannahhenry@users.noreply.github.com>
2020-04-07 12:31:32 +0100

    updating the display profile

--- lib/screens/display_profile_info.dart
+++ lib/screens/display_profile_info.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:Healthy_Ageing/utilities/constants.dart';
 
 String dog_name = "Oscar";
 String person_first_name = "Jake";
@@ -30,9 +29,103 @@ class display_profile_state extends State<display_profile_screen>{
               )
             ),
           ),
-        ],
-      )
+          AppBar(
+            backgroundColor: Colors.blueAccent,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () => Navigator.pop(context),
+            ),
+            elevation: 0,
+          ),
 
-    );
+          Container(
+            margin: const EdgeInsets.only(top: 80),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 30.0),
+                Expanded(
+                  child: Stack(
+                    children: <Widget>[
+                      Container(
+                        height: 400,
+                        width: 400,
+                        margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 10.0, bottom: 0),
+
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image(image: AssetImage('assests/mananddog.jpg'), fit: BoxFit.cover),
+                        )
+                      ),
+                      Container(
+                        alignment: Alignment.topCenter,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+                          decoration: BoxDecoration(
+                            color:Colors.lightBlueAccent,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+
+                          child: Text("0.5km away", style: TextStyle(fontSize: 18.0, color: Colors.white)),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.only(top: 170),
+                          child: Container(
+                            child: Text(dog_name + " & " + person_first_name + " " + person_last_name,
+                              style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30.0
+                              )
+                            ),
+                          )
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(top: 520),
+                            child: Icon(Icons.info_outline, color: Colors.lightBlueAccent, size:30),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 520, left: 10),
+                            child: Text(bio,
+                            style: TextStyle(
+                              color: Colors.lightBlueAccent,
+                              fontSize: 24,
+                            ),),
+                          )
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(top: 570, right: 5),
+                            child: Icon(Icons.location_on, color: Colors.grey, size: 40),
+                          ),
+
+                        Container(
+                          margin: EdgeInsets.only(top: 570, left: 7),
+                          child: Text(location_text,
+                          style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.grey.shade600
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ] ,
+                  ),
+
+            )
+          ]
+            ),
+      )
+      ]
+    ),);
   }
 }
